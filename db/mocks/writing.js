00f4014aa377244af86b42a21ec25051f5797dea
Adrian De Smul <adriandesmul@gmail.com>
2018-09-15 18:35:49 -0500

    Created writing mock

--- db/mocks/writing.js
+++ db/mocks/writing.js
@@ -1,18 +1,30 @@
-var pool = null;
+var competitionYear = process.env.YEAR;
 
-function readWriting() {
+function readWriting(entryType, user_id, cb) {
   console.log('- - - MOCK - - -')
   console.log('Read writing');
+  cb(false, {
+    "submission_id": 1,
+    "user_id": 1,
+    "title": "Title",
+    "body": "<p>Hello - this is my sijo yosup!</p>",
+    "type": "sijo",
+    "year": 2018
+  });
 }
 
-function updateWriting() {
+function saveWriting(title, body, entryType, user_id, cb) {
   console.log('- - - MOCK - - -')
   console.log('Update writing');
+  console.log('Title: ', title);
+  console.log('Body: ', body);
+  console.log('Type: ', entryType);
+  cb(false, 'Submission save successful')
 }
 
 module.exports = function() {
   return {
     read: readWriting,
-    update: updateWriting
+    save: saveWriting
   }
 }
