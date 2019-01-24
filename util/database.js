037df714b814deb7b68d5814ad0869d0dc7649ad
Adrian De Smul <adriandesmul@gmail.com>
2019-01-24 12:11:48 -0600

    Added database update

---
+++ util/database.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+
+let env = 'local';
+for (let item in process.argv) {
+  if (process.argv[item].indexOf('mode') != -1) {
+    env = process.argv[item].split('=')[1]
+  }
+}
+
+require('dotenv').config({
+  path: path.resolve(process.cwd(), env + '.env')
+});
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  connectionLimit: 10,
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_NAME,
+});
+
+const toLoad = [
+  'demographics.sql',
+  'users.sql',
+  'writing.sql'
+]
+
+toLoad.forEach(file => {
+  console.log(file);
+  fs.readFile(path.join('./db/sql', file), (err, data) => {
+    console.log("Getting SQL for " + path.join('./db/sql', file))
+    connection.query(data.toString(), (err, res) => {
+      console.log(err);
+      console.log(res);
+    })
+  })
+})
